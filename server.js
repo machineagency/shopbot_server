1c2e4b6263e08672b36937d6060737848014a08b
Jasper O'Leary <jhaazpr@gmail.com>
2022-07-12 12:54:17 -0700

    express serves whole directory as static files

--- server.js
+++ server.js
@@ -10,10 +10,9 @@ var browser_client;
 var clients = [];
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-	.use((req, res) => res.sendFile(INDEX))
+    .use(express.static(__dirname))
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({
